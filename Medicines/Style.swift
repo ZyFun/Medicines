6d494ee6312540aa485ad0789fcf0c425a5cb84f
Dmitriy Danilin <danilindn@icloud.com>
2020-11-12 17:53:46 +0300

    Создание макета приложения: Стадия 1

---
+++ Medicines/Style.swift
@@ -0,0 +1,11 @@
+//
+//  Style.swift
+//  Medicines
+//
+//  Created by Дмитрий Данилин on 12.11.2020.
+//
+
+import UIKit
+
+// Цвет для заливки приложения
+let colorBackground = #colorLiteral(red: 0.2941176471, green: 0.8182659943, blue: 0.8196078431, alpha: 1)
