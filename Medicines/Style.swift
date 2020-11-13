56b45de0e30daa15d0f4d9cdc05c4e7cb24c5867
Dmitriy Danilin <danilindn@icloud.com>
2020-11-13 16:45:19 +0300

    Изменил дизайн Добавил иконки Добавил скрин окна загрузки Отключил поддержку айпада

--- Medicines/Style.swift
+++ Medicines/Style.swift
@@ -8,5 +8,5 @@
 import UIKit
 
 // Цвет для заливки приложения
-let colorBackground = #colorLiteral(red: 0.2941176471, green: 0.8182659943, blue: 0.8196078431, alpha: 1) // color literal (Пока я новичек, чтобы не забыть как вызвать выбор цвета)
-let colorSelected = #colorLiteral(red: 0.2094576955, green: 0.5830765367, blue: 0.5847329497, alpha: 1)
+let colorBackground = #colorLiteral(red: 0.4087411761, green: 0.8173761368, blue: 0.6826727986, alpha: 1) // color literal (Пока я новичек, чтобы не забыть как вызвать выбор цвета)
+let colorSelected = #colorLiteral(red: 0.4351217747, green: 0.6936474442, blue: 0.6023892164, alpha: 1)
