9070eb339eee60123ea5dcec5ab8bdd8101dc6a2
Dmitriy Danilin <danilindn@icloud.com>
2020-12-03 19:47:50 +0300

    Немного задокументировал код

--- Medicines/Controller/FirstStartManager.swift
+++ Medicines/Controller/FirstStartManager.swift
@@ -11,7 +11,7 @@ import RealmSwift
 // TODO: Реализовать подобное, но для единичного запуска при первой установке приложения
 func learningNewObjectGet() {
     var learningMedicines: Results<Medicine>!
-    // Инициализируем переменную с объектами базы данных и делаем запрос этих объектов из базы данных
+    // Инициализируем переменную с объектами базы данных и делаем запрос этих объектов из базы данных. Для того, чтобы понять пустая база или нет. И на основе этого создать тестовый учебный объект
     learningMedicines = realm.objects(Medicine.self)
     if learningMedicines.isEmpty {
         let image = UIImage(named: "noImage")
