8ba0e2e8d30f22dc9c7b03b01e9847d06dce7e99
Dmitriy Danilin <danilindn@icloud.com>
2021-01-11 12:45:27 +0300

    Подготовка к созданию визуального окна приветствия, в замен окна сделанного кодом.

---
+++ Medicines/ViewControllers/WelcomePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomePageViewController.swift
+//  Medicines
+//
+//  Created by Дмитрий Данилин on 11.01.2021.
+//
+
+import UIKit
+
+class WelcomePageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
