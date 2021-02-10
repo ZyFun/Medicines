14bfe942de73418ff6495af3c8c3347fd647b208
Dmitriy Danilin <danilindn@icloud.com>
2021-02-10 22:13:41 +0300

    feat: Добавлена функция для автоматической смены оформления
    Полностью еще не доделано. На данный момент функция только сделана но еще нигде не используется.
    Остаётся доделать оформление цветовой схемы и внедрить всё это в приложение.
    По хорошему необходимо реализовать пункт меню с выбором нужной темы для более старых версий iOS

--- Medicines/Helpers/StyleManager.swift
+++ Medicines/Helpers/StyleManager.swift
@@ -9,6 +9,7 @@
 // TODO: Переделать дизайн под карточки и использовать их белым цветом.
 // Подобрать палитры можно тут https://get-color.ru
 // Названия цветов искать тут https://colorscheme.ru/color-names.html
+// Пример кода взят отсюда https://habr.com/ru/company/bcs_company/blog/493096/
 
 import UIKit
 
@@ -50,3 +51,38 @@ class CustomColors {
     
 }
 
+extension UIColor {
+    
+    // MARK: - Light Style properties
+    /// Используется для фона. Умеренный аквамариновый
+    static let background = #colorLiteral(red: 0.4078431373, green: 0.8156862745, blue: 0.6823529412, alpha: 1)
+    
+    /// Используется для выделений. Вердепешевый
+    static let selected = #colorLiteral(red: 0.8156862745, green: 0.7882352941, blue: 0.4078431373, alpha: 1)
+    
+    /// Используется для объектов помеченных на удаление. Розовый антик
+    static let delete = #colorLiteral(red: 0.8156862745, green: 0.4823529412, blue: 0.4078431373, alpha: 1)
+    
+    /// Используется для иконок. Аметистовый
+    static let icon = #colorLiteral(red: 0.5176470588, green: 0.4078431373, blue: 0.8156862745, alpha: 1)
+    
+    // MARK: Цвета не подобранные палитрой
+    /// Используется  для шапок. Лиственный зеленый Крайола.
+    static let navigationBar = #colorLiteral(red: 0.4352941176, green: 0.6941176471, blue: 0.6039215686, alpha: 1)
+
+    /// Используется для объектов помеченных на удаление. Красно-оранжевый Крайола.
+//    let iconAppColor = #colorLiteral(red: 1, green: 0.3137254902, blue: 0.2862745098, alpha: 1)
+    
+    // MARK: - Functions
+    /// Метод назначает автоматическое переключение тёмной и светлой темы оформления, в зависимости от системных настроек. Для iOS ниже 13 версии, оформление всегда берется из светлого значения.
+    static func customColor(light: UIColor, dark: UIColor) -> UIColor {
+        if #available(iOS 13, *) {
+            return UIColor.init { traitCollection in
+                return traitCollection.userInterfaceStyle == .dark ? dark : light
+            }
+        } else {
+            return light
+        }
+    }
+    
+}
