14c2ad77cc65da8c6b41a118a46bb40dd4c12581
Dmitriy Danilin <danilindn@icloud.com>
2021-02-13 15:40:08 +0300

    feat: Добавил цвета для темной темы
    Добавлен enum с темной темой. Так же переместил светлое оформление в enum со светлой темой.

--- Medicines/Helpers/StyleManager.swift
+++ Medicines/Helpers/StyleManager.swift
@@ -54,24 +54,45 @@ class CustomColors {
 extension UIColor {
     
     // MARK: - Light Style properties
-    /// Используется для фона. Умеренный аквамариновый
-    static let background = #colorLiteral(red: 0.4078431373, green: 0.8156862745, blue: 0.6823529412, alpha: 1)
+    enum light {
+        /// Используется для фона. Умеренный аквамариновый
+        static let background = #colorLiteral(red: 0.4078431373, green: 0.8156862745, blue: 0.6823529412, alpha: 1)
+        
+        /// Используется для выделений. Вердепешевый
+        static let selected = #colorLiteral(red: 0.8156862745, green: 0.7882352941, blue: 0.4078431373, alpha: 1)
+        
+        /// Используется для объектов помеченных на удаление. Розовый антик
+        static let delete = #colorLiteral(red: 0.8156862745, green: 0.4823529412, blue: 0.4078431373, alpha: 1)
+        
+        /// Используется для иконок. Аметистовый
+        static let icon = #colorLiteral(red: 0.5176470588, green: 0.4078431373, blue: 0.8156862745, alpha: 1)
+        
+        // MARK: Цвета не подобранные палитрой
+        /// Используется  для шапок. Лиственный зеленый Крайола.
+        static let navigationBar = #colorLiteral(red: 0.4352941176, green: 0.6941176471, blue: 0.6039215686, alpha: 1)
+        
+        /// Используется для объектов помеченных на удаление. Красно-оранжевый Крайола.
+    //    let iconAppColor = #colorLiteral(red: 1, green: 0.3137254902, blue: 0.2862745098, alpha: 1)
+    }
     
-    /// Используется для выделений. Вердепешевый
-    static let selected = #colorLiteral(red: 0.8156862745, green: 0.7882352941, blue: 0.4078431373, alpha: 1)
-    
-    /// Используется для объектов помеченных на удаление. Розовый антик
-    static let delete = #colorLiteral(red: 0.8156862745, green: 0.4823529412, blue: 0.4078431373, alpha: 1)
-    
-    /// Используется для иконок. Аметистовый
-    static let icon = #colorLiteral(red: 0.5176470588, green: 0.4078431373, blue: 0.8156862745, alpha: 1)
-    
-    // MARK: Цвета не подобранные палитрой
-    /// Используется  для шапок. Лиственный зеленый Крайола.
-    static let navigationBar = #colorLiteral(red: 0.4352941176, green: 0.6941176471, blue: 0.6039215686, alpha: 1)
-
-    /// Используется для объектов помеченных на удаление. Красно-оранжевый Крайола.
-//    let iconAppColor = #colorLiteral(red: 1, green: 0.3137254902, blue: 0.2862745098, alpha: 1)
+    // MARK: - Dark Style properties
+    enum dark {
+        /// Используется для фона. Ядовито-зеленый.
+        static let background = #colorLiteral(red: 0.2274509804, green: 0.4549019608, blue: 0.3843137255, alpha: 1)
+        
+        /// Используется для выделений. Бледно-коричневый.
+        static let selected = #colorLiteral(red: 0.4509803922, green: 0.4, blue: 0.2823529412, alpha: 1)
+        
+        // MARK: Цвета не подобранные палитрой
+        /// Используется для иконок. Умеренный аспидно-синий.
+        static let icon = #colorLiteral(red: 0.5725490196, green: 0.4509803922, blue: 0.9058823529, alpha: 1)
+        
+        /// Используется для объектов помеченных на удаление. Сочный каштановый Крайола.
+        static let delete = #colorLiteral(red: 0.6941176471, green: 0.3098039216, blue: 0.2823529412, alpha: 1)
+        
+        /// Используется  для шапок. Сосновый зеленый
+        static let navigationBar = #colorLiteral(red: 0.1607843137, green: 0.3176470588, blue: 0.2666666667, alpha: 1)
+    }
     
     // MARK: - Functions
     /// Метод назначает автоматическое переключение тёмной и светлой темы оформления, в зависимости от системных настроек. Для iOS ниже 13 версии, оформление всегда берется из светлого значения.
